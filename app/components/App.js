20329bf65c457d33a83fb5070af75afe96053864
Artur Veselovski <artur.veselovski@hotmail.fi>
2017-06-22 10:36:15 +0300

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+// libs
+var React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,10 @@
+// libs
+var React = require('react');
+var ReactDOM = require('react-dom');
+// components
+var App = require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
